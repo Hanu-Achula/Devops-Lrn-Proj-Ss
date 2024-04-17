fe32dca6cda1b62b5bdaa364146daa0e9212136d
Hanu-Achula <hanumaiahachula1@gmail.com>
2024-04-17 11:15:51 +0530

    Fourth Version

--- variables.sh
+++ variables.sh
@@ -5,12 +5,9 @@
 # Date-Written   : 17-04-2024
 # Description    : To Learn Variables
 ###############################
-# Declaring Variables
-PERSON1=$1  #argument1
-PERSON2=$2  #argument2
 
-# Referencing Variables
-echo "$PERSON1:: Hi $PERSON2"
-echo "$PERSON2:: Hi $PERSON1,How Are You!"
-echo "$PERSON1:: I am Fine!How Are You?"
-echo "$PERSON2 :: I am Good!!"
+# Capturing User Entered Credentials
+read -ps "User Name:" USERNAME
+read -ps "Password:" PASSWORD
+# Print Credentials
+echo "User Name Is : $USERNAME Password Is : $PASSWORD"
