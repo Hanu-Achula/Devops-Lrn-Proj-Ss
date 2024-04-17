73ce11eed614b008c1ea59148aa856ddc9ad636c
Hanu-Achula <hanumaiahachula1@gmail.com>
2024-04-17 14:43:47 +0530

    Fourth Version

--- special.sh
+++ special.sh
@@ -4,5 +4,9 @@
 # Author    :   Hanu
 # Date      :   17-04-2024
 ##########################
+declare -i sum = ${1}
+
+echo "Sum Is ${sum}"
+
 sleep 60 &
 echo "${@} ${#} ${PWD} ${0} ${HOME} ${HOSTNAME} ${$} ${!}" 
