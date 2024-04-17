1ac182a7ba5c820b4d229496262697f6a270f178
Hanu-Achula <hanumaiahachula1@gmail.com>
2024-04-17 12:14:21 +0530

    Fourth Version

---
+++ array.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+##################################
+# Author      : Hanu
+# Date        : 17-04-2024
+# Desc        : Array Learn
+###################################
+
+# Array Defined
+
+movies=("Murari" "DJ Tilu" "Sagaram")
+
+# First Element
+
+echo "First Element ${movies[0]}"
+
+# All Elements
+
+echo "All Elements ${movies[@]}"
