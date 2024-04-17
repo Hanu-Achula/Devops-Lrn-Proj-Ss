12261910cb87c728198376c63bb721a98de97bf6
Hanu-Achula <hanumaiahachula1@gmail.com>
2024-04-17 08:37:54 +0530

    Second Version

---
+++ variables.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#############################
+# Author         : Hanu
+# Date-Written   : 17-04-2024
+# Description    : To Learn Variables
+###############################
+
+echo "Hanu:: Hi Tej"
+echo "Tej:: Hi Hanu,How Are You!"
+echo "Hanu:: I am Fine!How Are You?"
+echo "Tej :: I am Good!!"
