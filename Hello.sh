24cc45f657c256aad6b52a4f1c02a6b8ff09b727
Hanu-Achula <hanumaiahachula1@gmail.com>
2024-04-17 08:14:54 +0530

    First Version

---
+++ Hello.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+##############################
+# Author         : Hanu
+# Date-Written   : 17-04-2024
+# Description    : To print hello message
+###############################
+
+echo "Hello,World!!!!"  # echo is for print on console
