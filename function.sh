c3e8cdc905d92583e2b7aa9a2b70fb750e195fd6
Hanu-Achula <hanumaiahachula1@gmail.com>
2024-04-18 18:10:00 +0530

    Second Version

--- function.sh
+++ function.sh
@@ -3,9 +3,9 @@ USER=$(id -u)
 R ="/e[31m"
 N ="/e[0m"
 
-if [ $USER -ne 0]
+if [ $USER -ne 0 ]
  then
- echo -e "R Script Should be Run With Admin N"
+ echo -e "${R} Script Should be Run With Admin ${N}"
  else
  echo "YOU ARE SUPER USER"
  fi
