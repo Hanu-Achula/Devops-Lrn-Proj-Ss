2dde201c874c0bfc3571fa319ac82d75d3a2f592
Hanu-Achula <hanumaiahachula1@gmail.com>
2024-04-18 17:26:11 +0530

    First Version

--- special.sh
+++ special.sh
@@ -1,12 +1,3 @@
 #!/bin/bash
 
-##########################
-# Author    :   Hanu
-# Date      :   17-04-2024
-##########################
-declare -i sum=${1}
-
-echo "Sum Is ${sum}"
-
-sleep 60 &
-echo "${@} ${#} ${PWD} ${0} ${HOME} ${HOSTNAME} ${$} ${!}" 
+echo "The Total Arguments ${@}"
