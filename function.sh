df7a19668ee6a71d81447027239e4a42b5bcf11e
Hanu-Achula <hanumaiahachula1@gmail.com>
2024-04-18 18:07:13 +0530

    Second Version

--- function.sh
+++ function.sh
@@ -1,6 +1,11 @@
-message()
-{
-    echo "${1} ${2}"
-}
+USER=$(id -u)
 
-message HELLO WORLD
+R ="/e[31m"
+N ="/e[0m"
+
+if [ $USER -ne 0]
+ then
+ echo -e "R Script Should be Run With Admin N"
+ else
+ echo "YOU ARE SUPER USER"
+ fi
